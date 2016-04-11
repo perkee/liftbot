07be7804f770f06b6e46aef3b269bd64fc1bb3f5
perkee <hello@perk.ee>
2016-04-11 18:59:10 -0400

    better comments in test file and testing PRs command

--- tests/CommandsFromSlackTest.php
+++ tests/CommandsFromSlackTest.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 
 class CommandsFromSlackTest extends TestCase
 {
+    /**
+     * Given a string, return a request with that string in the text field
+     *
+     * @param string $text value of text field in request
+     * @return Illuminate\Http\Request Reequest object with minumum field required for middleware
+     */
     private function requestWithText($text = ''){
         $request = new Illuminate\Http\Request();
         $request->replace([
@@ -16,7 +22,7 @@ class CommandsFromSlackTest extends TestCase
         return $request;
     }
     /**
-     * A basic test example.
+     * Test that the lift command is set correctly for typical lift commands.
      *
      * @return void
      */
@@ -35,6 +41,31 @@ class CommandsFromSlackTest extends TestCase
             $mw = new \App\Http\Middleware\GetCommandFromSlack;
             $mw->handle($request,function($r) use ($after){
                 $this->assertEquals('lift',$r->input('command'));
+                $this->assertEquals($after,$r->input('text'));
+            });
+        }
+    }
+
+    /**
+     * Test that the PRs command is set correctly for typical lift commands.
+     *
+     * @return void
+     */
+    public function testShouldKnowPRsCommand()
+    {
+        $texts = [ //text from before $mw => text after mw
+            '  PRs   @mr-ms-nikhil-thomas  '
+                => '@mr-ms-nikhil-thomas',
+            'prs' => '',
+            ' PRS ' => '',
+            ' prS'  => '',
+            'prs    @perkee '  => '@perkee'
+        ];
+        foreach ($texts as $before => $after) {
+            $request = $this->requestWithText($before);
+            $mw = new \App\Http\Middleware\GetCommandFromSlack;
+            $mw->handle($request,function($r) use ($after){
+                $this->assertEquals('prs',$r->input('command'));
                 $this->assertEquals($after,$r->input('text'));
             });
         }
